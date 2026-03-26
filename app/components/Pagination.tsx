225c781123aad9a21b38303316e1485265c97a24
kalan <indulakalana@gmail.com>
2026-03-26 19:10:47 +0530

    prev and next button function added

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -11,8 +11,14 @@ const Pagination = ({page,count}:{page:number;count:number}) => {
     const searchParams = useSearchParams();
 
     const currentPage = Number(searchParams.get('page') || page || 1);
+    const totalPages = Math.ceil(count / ITEM_PER_PAGE);
+    const isPrevDisabled = currentPage <= 1;
+    const isNextDisabled = currentPage >= totalPages;
 
     const changePage = (newPage:number) => {
+
+        if (newPage < 1 || newPage > totalPages) return;
+
         //fetch existing params
         const params=new URLSearchParams(window.location.search);
         params.set("page",newPage.toString());
@@ -21,14 +27,15 @@ const Pagination = ({page,count}:{page:number;count:number}) => {
 
     return (
         <div className="p-4 flex items-center justify-between text-gray-500">
-            <button disabled={true}
-                    className={`py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed`}
+            <button disabled={isPrevDisabled}
+                    onClick={()=>changePage(currentPage - 1)}
+                    className={`py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer`}
             >
                 Prev
             </button>
             <div className="flex items-center gap-2 text-sm">
                 {Array.from(
-                    {length:Math.ceil(count/ITEM_PER_PAGE)},
+                    {length:totalPages},
                     (_, index) => {
                         const pageIndex= index + 1;
                         return (
@@ -45,7 +52,11 @@ const Pagination = ({page,count}:{page:number;count:number}) => {
                     }
                 )}
             </div>
-            <button disabled={true} className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+            <button
+                disabled={isNextDisabled}
+                onClick={() => changePage(currentPage + 1)}
+                className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+            >
                 Next
             </button>
         </div>
